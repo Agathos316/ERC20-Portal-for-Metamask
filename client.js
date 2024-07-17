db28da581a5b5ca6de3653be4a074db68fa23691
Agathos <138493133+Agathos316@users.noreply.github.com>
2024-07-17 18:02:57 +1000

    Update client.js

--- client.js
+++ client.js
@@ -66,7 +66,7 @@ const $txLog4 = document.getElementById('txLog4');
  */
 // Detect which provider to use for metamask. Necessary because 'window.ethereum' is not stable.
 // It works sometimes and sometimes does not, in the same browser.
-if ((window.ethereum).isMetaMask) {
+if (window.ethereum && window.ethereum.isMetaMask) {
     web3 = new Web3(window.ethereum);
 } else {
     web3 = new Web3(window.web3.currentProvider); // For legacy browsers
