18e08b11317924c9aae0bb4f1f2de7ee4a331686
Agathos <138493133+Agathos316@users.noreply.github.com>
2024-07-17 17:56:44 +1000

    Update client.js

--- client.js
+++ client.js
@@ -66,12 +66,11 @@ const $txLog4 = document.getElementById('txLog4');
  */
 // Detect which provider to use for metamask. Necessary because 'window.ethereum' is not stable.
 // It works sometimes and sometimes does not, in the same browser.
-/*if ((window.ethereum).isMetaMask) {
+if ((window.ethereum).isMetaMask) {
     web3 = new Web3(window.ethereum);
 } else if ((window.web3.currentProvider).isMetaMask) {
     web3 = new Web3(window.web3.currentProvider); // For legacy browsers
-}*/
-web3 = new Web3(window.ethereum);
+}
 await web3.eth.getAccounts()    // Or use 'window.ethereum.request({ method: 'eth_accounts' })'.
 .then((accounts) => {
     // If wallet connected, then initialize dapp with account information.
